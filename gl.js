4dbbbda4e98f51debebefa11eb3e2447e0635176
ashisku8 <82260038+ashisku8@users.noreply.github.com>
2023-05-21 01:03:13 +0530

    Add files via upload

--- gl.js
+++ gl.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 	$('#abt').show();
 	$('#cont').hide();
+	$('#serv').hide();
 
 	$('#contact').click(function() {
 
 		$('#abt').hide();
+		
+		$('#serv').hide();
 
 		$('#cont').show();
+
+	});
+	
+		$('#service').click(function() {
+
+		$('#abt').hide();
+
+		$('#cont').hide();
+		
+		$('#serv').show();
 
 	});
 
@@ -15,6 +28,8 @@ $(document).ready(function() {
 		$('#abt').show();
 
 		$('#cont').hide();
+		
+		$('#serv').hide();
 
 	});
 
